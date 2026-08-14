763ede1e9810b4abca55b47b0cb22ac6f00688f8
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2026-08-14 11:14:49 -0700

    initilise Project

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ success: true, message: 'Chatter API is running' });
+});
+
+const PORT = 3000;
+
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
